eb02f71813ae796efbd1003a23a11ab5156d39b9
Hongcai Deng <admin@dhchouse.com>
2024-12-06 00:09:28 +0800

    test: 💍 add zipfs

--- src/fs.ts
+++ src/fs.ts
@@ -4,6 +4,18 @@ import { S_IFDIR, S_IFREG } from '@zenfs/core/emulation/constants.js';
 import { Errno, ErrnoError } from '@zenfs/core/error.js';
 import { FileSystem } from '@zenfs/core/filesystem.js';
 import { Readonly } from '@zenfs/core/mixins/readonly.js';
+import { join } from 'path';
+
+export interface DownloadZipOptions {
+	/**
+	 * Base URL for the registry
+	 */
+	baseUrl: string;
+	/**
+	 * Package size threshold for downloading zip files
+	 */
+	packageSizeThreshold?: number;
+}
 
 /**
  * Configuration options for a UnpkgFS file system.
@@ -17,6 +29,10 @@ export interface UnpkgOptions {
 	 * Unpkg site URL
 	 */
 	baseUrl: string;
+	/**
+	 * Download zip files instead of individual files
+	 */
+	downloadZip?: DownloadZipOptions;
 }
 
 interface UnpkgFileMeta {
@@ -38,10 +54,43 @@ interface UnpkgFileMeta {
 	files?: UnpkgFileMeta[];
 }
 
+class FsIndex extends Map<String, Stats> {}
+
+function parsePackageName(path: string): string {
+	const match = path.match(/(\/)(@[a-z0-9-~][a-z0-9-._~]*)/);
+	if (match) {
+		return match[2];
+	}
+	return '';
+}
+
 export class UnpkgFS extends Readonly(Async(FileSystem)) {
 	_sync = InMemory.create({ name: 'unpkg-tmpfs' });
 
 	private _fetch: typeof fetch;
+
+	private _index = new FsIndex();
+
+	private _packagesOfZipFs = new Map<string, FileSystem>();
+
+	private parseFileMetaAndSetToIndex(path: string, fileMeta: UnpkgFileMeta): void {
+		if (fileMeta.type === 'directory') {
+			const stats = new Stats({
+				mode: 0o555 | S_IFDIR,
+				size: 4096,
+			});
+			this._index.set(fileMeta.path, stats);
+			fileMeta.files?.forEach(file => {
+				this.parseFileMetaAndSetToIndex(join(path, fileMeta.path), file);
+			});
+		} else {
+			const stats = new Stats({
+				mode: 0o555 | S_IFREG,
+				size: fileMeta.size,
+			});
+			this._index.set(path, stats);
+		}
+	}
 
 	constructor(private readonly options: UnpkgOptions) {
 		super();
@@ -60,7 +109,6 @@ export class UnpkgFS extends Readonly(Async(FileSystem)) {
 			const fileMeta = (await res.json()) as UnpkgFileMeta;
 			return fileMeta;
 		} catch (err) {
-			console.error(err);
 			throw new ErrnoError(Errno.EBUSY, path);
 		}
 	}
@@ -91,17 +139,23 @@ export class UnpkgFS extends Readonly(Async(FileSystem)) {
 				size: 4096,
 			});
 		}
+		if (this._index.has(path)) {
+			return this._index.get(path)!;
+		}
 		const fileMeta = await this.fetchFileMeta(path);
+		this.parseFileMetaAndSetToIndex(path, fileMeta);
 		if (fileMeta.type === 'directory') {
-			return new Stats({
+			const stats = new Stats({
 				mode: 0o555 | S_IFDIR,
 				size: 4096,
 			});
+			return stats;
 		} else {
-			return new Stats({
+			const stats = new Stats({
 				mode: 0o555 | S_IFREG,
 				size: fileMeta.size,
 			});
+			return stats;
 		}
 	}
 	async openFile(path: string, flag: string): Promise<File> {
@@ -134,6 +188,18 @@ const _Unpkg = {
 		baseUrl: {
 			type: 'string',
 			required: true,
+		},
+		downloadZip: {
+			type: 'object',
+			validator(opt: DownloadZipOptions) {
+				if (typeof opt.packageSizeThreshold !== 'number') {
+					throw new Error('packageSizeThreshold must be a number');
+				}
+				if (typeof opt.baseUrl !== 'string') {
+					throw new Error('baseUrl must be a string');
+				}
+			},
+			required: false,
 		},
 	},
 
